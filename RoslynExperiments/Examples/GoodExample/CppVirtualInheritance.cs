ff0b9b64ba3c8ff1c5cba9c8c407d2d39b2f8a41
stdstring <std_string@mail.ru>
2019-07-12 16:48:16 +0500

    added good & bad example projects

---
+++ RoslynExperiments/Examples/GoodExample/CppVirtualInheritance.cs
@@ -0,0 +1,7 @@
+﻿namespace CsToCppPorter
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Interface)]
+    public class CppVirtualInheritance : System.Attribute
+    {
+    }
+}
